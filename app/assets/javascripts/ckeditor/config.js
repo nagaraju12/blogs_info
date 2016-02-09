bdc7517fd71a8b852e0a03939c20794f003f0cfd
rails <rails@railsnew.com>
2016-02-09 18:02:24 +0530

    create a blogs

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+  // ... other configuration ...
+
+  config.toolbar_mini = [
+    ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+  ];
+  config.toolbar = "simple";
+
+  // ... rest of the original config.js  ...
+}
